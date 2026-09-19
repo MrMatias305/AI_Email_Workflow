e58bd7e8a30abfeecf22222223350382d844d93a
MrMatias305 <matiasmayor22xxii@gmail.com>
2026-09-19 11:53:25 +0530

    Add gmail workflow

---
+++ gmail.py
@@ -0,0 +1,43 @@
+import os
+from unittest import result
+
+from google.auth.transport import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+
+def get_gmail_service():
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return build('gmail', 'v1', credentials=creds)
+
+
+if __name__ == '__main__':
+    gmail_service = get_gmail_service()
+
+    results = gmail_service.users().messages().list(
+        userId='me',
+        maxResults=5
+    ).execute()
+
+    messages = results.get('messages', [])
+
+    print(f"Found {len(messages)} messages.")
+    for message in messages:
+        print(message['id'])
